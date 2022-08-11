cc18dc7fe310ee64f15dc1fe4fe947e0e1ac364d
federicofdz95 <federicofdz95@gmail.com>
2022-08-11 13:24:12 -0300

    1

---
+++ secciones/descargar.php
@@ -0,0 +1,11 @@
+    <section class="resume-section" id="descargar">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Descargar</h2>
+            <div class="subheading mb-3">Elija el formato del archivo</div>
+            <ul class="list-inline dev-icons">
+                <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+            </ul>            
+        </div>
+    </section>
